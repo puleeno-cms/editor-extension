73957a5b9cab42beff09a7f8ae53d0d05612e4e4
Puleeno Nguyen <puleeno@gmail.com>
2023-08-24 20:28:59 +0700

    Create composer files

---
+++ libs/composer/installed.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4f5fcc0270aa0bf16dd784414de1acc7aede820f',
+        'name' => 'puleeno-cms/text-editor',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'puleeno-cms/text-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4f5fcc0270aa0bf16dd784414de1acc7aede820f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
